f6570779bc537180b4aecdc6971ba9810c956ada
MaksimSOkur <maksim_sokur@mail.ru>
2015-04-07 21:09:42 +0300

    Tutorial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        out.println("Hello");
+        System.out.println("Hello");
         //good day
 	// write your code here
     }
